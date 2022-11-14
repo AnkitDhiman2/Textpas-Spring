4abc582586c4b071f5b8d57869023ecd4faa3185
Ankit Kumar <ankitdkumar43@gmail.com>
2022-11-14 12:59:56 +0530

    First Commit

---
+++ Textpad-Spring/src/main/java/com/project/TextpadSpring/TextpadSpringApplication.java
@@ -0,0 +1,13 @@
+package com.project.TextpadSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextpadSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextpadSpringApplication.class, args);
+	}
+
+}
